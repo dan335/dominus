74411cb5a3828a55f37b3137b40a9e344a9f3116
Dan <danphi@gmail.com>
2017-09-30 09:48:19 -0700

    fix for people with 0 rankings.  limit people on ranking page

--- packages/dominus-landing/landingRankings/server/landingRankingsPublish.js
+++ packages/dominus-landing/landingRankings/server/landingRankingsPublish.js
@@ -1,11 +1,11 @@
 Meteor.publish('overallRankingsRegular', function() {
-  let query = Meteor.users.find({"rankingRegular.numGames": {$gt:0}, "rankingRegular.overallRank": {$gt:0}}, {fields:{username:1, rankingRegular:1}}, {disableOplog:true, pollingIntervalMs:1000*60*30})
+  let query = Meteor.users.find({"rankingRegular.numGames": {$gt:0}, "rankingRegular.overallRank": {$gt:0}}, {limit:100, fields:{username:1, rankingRegular:1}}, {disableOplog:true, pollingIntervalMs:1000*60*30})
   Mongo.Collection._publishCursor(query, this, 'overallrankingsregular')
   return this.ready();
 });
 
 Meteor.publish('overallRankingsPro', function() {
-  let query = Meteor.users.find({"rankingPro.numGames": {$gt:0}, "rankingPro.overallRank": {$gt:0}}, {fields:{username:1, rankingPro:1}}, {disableOplog:true, pollingIntervalMs:1000*60*30})
+  let query = Meteor.users.find({"rankingPro.numGames": {$gt:0}, "rankingPro.overallRank": {$gt:0}}, {limit:100, fields:{username:1, rankingPro:1}}, {disableOplog:true, pollingIntervalMs:1000*60*30})
   Mongo.Collection._publishCursor(query, this, 'overallrankingspro')
   return this.ready();
 })
