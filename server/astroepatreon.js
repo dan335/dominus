fef0fff1b94ba9569f5bd382a927b72e0b51dda9
Dan <danphi@gmail.com>
2018-04-29 15:39:13 -0700

    add debug info

--- server/astroepatreon.js
+++ server/astroepatreon.js
@@ -1,5 +1,7 @@
 Meteor.methods({
   astroepatreon() {
-    HTTP.get('http://astroe.io/patreonnotify');
+    HTTP.get('http://astroe.io/patreonnotify', {}, (error, result) => {
+      console.log(result);
+    });
   }
 })
