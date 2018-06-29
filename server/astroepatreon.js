2a112cd5dec8901c1ef96c0dd7130bc767dc2d18
Dan <danphi@gmail.com>
2018-06-29 00:22:43 -0700

    add dominus 2 to patreon sync

--- server/astroepatreon.js
+++ server/astroepatreon.js
@@ -6,5 +6,12 @@ Picker.route('/astroepatreon', function(params, req, res, next) {
       console.log('notifying astroe of patreon update');
     }
   });
+  HTTP.get('http://dominus2.io/patreonsync', {}, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('notifying dominus 2 of patreon update');
+    }
+  });
   res.end();
 });
