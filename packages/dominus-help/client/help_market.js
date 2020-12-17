bb920e03d0e4d9895e3b6cdf1d65da9725fef038
SolitudalDeveloper <adstronach@gmail.com>
2020-12-17 21:56:41 +0000

    Update help_market.js
    Income interval converted into minutes from ms

--- packages/dominus-help/client/help_market.js
+++ packages/dominus-help/client/help_market.js
@@ -12,6 +12,6 @@ Template.help_market.helpers({
   },
 
   incomeInterval: function() {
-    return _gs.castles(Session.get('gameId'), 'incomeInterval') / 60 ; /* ms to seconds */
+    return Math.round(_gs.castles(Session.get('gameId'), 'incomeInterval') / 1000 / 60);
   }
 })
