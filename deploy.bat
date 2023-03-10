c2c3c2ad44331799ac276d1ce0abbe9aaa147f70
Dan <danphi@gmail.com>
2023-03-10 09:14:35 -0500

    analytics

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 docker login registry.gitlab.com -u danphi
-docker build -t registry.gitlab.com/danphi/dominus .
+docker build -t registry.gitlab.com/danphi/dominus --platform linux/amd64 .
 docker push registry.gitlab.com/danphi/dominus
 ssh root@dominusgame.net "cd /shipyard/compose; docker-compose pull; docker-compose up -d; docker-compose restart nginx;"
