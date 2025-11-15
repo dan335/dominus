08e6306ee860b215cbca45552993ad95e20c9bb3
Dan <danphi@gmail.com>
2025-11-15 09:09:21 -0500

    asdf

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker login registry.gitlab.com -u danphi
 rm -rf .build
 meteor build .build --architecture os.linux.x86_64
-docker build -t registry.gitlab.com/danphi/dominus --platform linux/amd64 .
+docker build -t registry.gitlab.com/danphi/dominus --platform linux/x86_64 .
 docker push registry.gitlab.com/danphi/dominus
-ssh root@dominusgame.net "cd /shipyard/compose; docker-compose pull; docker-compose up -d; docker-compose restart nginx;"
+ssh dan@104.236.39.83 "cd ~/server; docker-compose pull; docker-compose up -d;"
